1e7253256fdffe40c561d27d87c11ebbc7c75fdf
awb99 <awb99@gmail.com>
2022-12-01 20:22:05 +0100

    mailgun send

--- mailgun-send.sh
+++ mailgun-send.sh
@@ -4,10 +4,10 @@ echo api:$MAILGUN_API_KEY
 
 curl -s --user api:$MAILGUN_API_KEY \
 https://api.mailgun.net/v3/crbclean.com/messages \
--F from='Mailgun Sandbox <postmaster@crbclean.com>' \
+-F from='Mongo Marketing <info@crbclean.com>' \
 -F to='internal@crbclean.com' \
 -F subject='Hello' \
 -F template='test' \
 -F t:variables='{"title": "API documentation", "body": "Sending messages with templates"}'
 
-# 'Mongo Marketing <info@crbclean.com>' \
+# -F from='Mailgun Sandbox <postmaster@crbclean.com>' \

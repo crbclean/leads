5a7303903a4276c0029ecbf0c9d38c5dc0f213fd
awb99 <awb99@gmail.com>
2022-12-01 20:15:27 +0100

    mailgun send added

---
+++ mailgun-send.sh
@@ -0,0 +1,13 @@
+
+
+echo api:$MAILGUN_API_KEY
+
+curl -s --user api:$MAILGUN_API_KEY \
+https://api.mailgun.net/v3/crbclean.com/messages \
+-F from='Mailgun Sandbox <postmaster@crbclean.com>' \
+-F to='internal@crbclean.com' \
+-F subject='Hello' \
+-F template='test' \
+-F t:variables='{"title": "API documentation", "body": "Sending messages with templates"}'
+
+# 'Mongo Marketing <info@crbclean.com>' \

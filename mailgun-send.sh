6cd71c7a7229c704b7475facdb144fbef0c46219
awb99 <awb99@gmail.com>
2023-01-16 19:01:33 +0100

    mailchimp

--- mailgun-send.sh
+++ mailgun-send.sh
@@ -1,13 +1,14 @@
 
 
+# mailgun api creds of florian are in private gist:
+# https://gist.github.com/awb99/
+
 echo api:$MAILGUN_API_KEY
 
 curl -s --user api:$MAILGUN_API_KEY \
 https://api.mailgun.net/v3/crbclean.com/messages \
--F from='Mongo Marketing <info@crbclean.com>' \
--F to='internal@crbclean.com' \
--F subject='Hello' \
--F template='test' \
+-F from='David Araya <davida@crbclean.com>' \
+-F to='test@marketing.crbclean.com' \
+-F subject='christmas special 2022' \
+-F template='christmas1' \
 -F t:variables='{"title": "API documentation", "body": "Sending messages with templates"}'
-
-# -F from='Mailgun Sandbox <postmaster@crbclean.com>' \
